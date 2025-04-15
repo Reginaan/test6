a7c8d13e0c13071d370594e92e917b2d79c5d4e9
Regina Belova <reginabelova@bk.ru>
2025-04-15 08:02:00 +0300

    Исправила пути

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import avatar from '@assets/icons/avatar.svg';
-import './Header.css';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import avatar from "@assets/icons/avatar.svg";
+import "./Header.css";
 
 export const Header = () => {
   const location = useLocation();
@@ -15,34 +15,34 @@ export const Header = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const getTitle = () => {
     switch (location.pathname) {
-      case '/':
-        return 'Главная';
-      case '/feed':
-        return 'Лента';
-      case '/channels':
-        return 'Каналы';
-      case '/video':
-        return 'Видео';
-      case '/save':
-        return 'Сохраненное';
-      case '/bells':
-        return 'Уведомления';
-      case '/chat':
-        return 'Чаты';
+      case "/":
+        return "Главная";
+      case "/feed":
+        return "Лента";
+      case "/channels":
+        return "Каналы";
+      case "/video":
+        return "Видео";
+      case "/save":
+        return "Сохраненное";
+      case "/bells":
+        return "Уведомления";
+      case "/chat":
+        return "Чаты";
       default:
-        return 'МТ.РУ';
+        return "МТ.РУ";
     }
   };
 
   return (
-    <header className={`header ${hideHeader ? 'header--hidden' : ''}`}>
+    <header className={`header ${hideHeader ? "header--hidden" : ""}`}>
       <div className="header-container">
         <img src={avatar} alt="avatar" />
         <h1>{getTitle()}</h1>
--- src/components/Sidebar/data/menu.ts
+++ src/components/Sidebar/data/menu.ts
@@ -1,21 +1,21 @@
-import feed from '@assets/icons/icon.svg';
-import channels from '@assets/icons/compass.svg';
-import video from '@assets/icons/video.svg';
-import save from '@assets/icons/bookmark.svg';
-import bells from '@assets/icons/bell.svg';
-import chat from '@assets/icons/chat.svg';
-import { MenuItem } from '@types';
+import feed from "@assets/icons/icon.svg";
+import channels from "@assets/icons/compass.svg";
+import video from "@assets/icons/video.svg";
+import save from "@assets/icons/bookmark.svg";
+import bells from "@assets/icons/bell.svg";
+import chat from "@assets/icons/chat.svg";
+import { MenuItem } from "../../../types";
 
 export const newsCounts = [
-  { path: 'chat', count: 1 },
-  { path: 'bells', count: 1 },
+  { path: "chat", count: 1 },
+  { path: "bells", count: 1 },
 ];
 
 export const menu: MenuItem[] = [
-  { iconSrc: feed, href: '/feed', label: 'Лента' },
-  { iconSrc: channels, href: '/channels', label: 'Каналы' },
-  { iconSrc: video, href: '/video', label: 'Видео' },
-  { iconSrc: save, href: '/save', label: 'Сохраненное' },
-  { iconSrc: bells, href: '/bells', label: 'Уведомления' },
-  { iconSrc: chat, href: '/chat', label: 'Чаты' },
+  { iconSrc: feed, href: "/feed", label: "Лента" },
+  { iconSrc: channels, href: "/channels", label: "Каналы" },
+  { iconSrc: video, href: "/video", label: "Видео" },
+  { iconSrc: save, href: "/save", label: "Сохраненное" },
+  { iconSrc: bells, href: "/bells", label: "Уведомления" },
+  { iconSrc: chat, href: "/chat", label: "Чаты" },
 ];

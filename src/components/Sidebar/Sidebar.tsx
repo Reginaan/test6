16c257a219f37083b17ef3bb03c356760e1f3b03
Regina Belova <reginabelova@bk.ru>
2025-04-15 20:56:47 +0300

    дополнения

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
-import { Link, useLocation } from 'react-router-dom';
-import logoSrc from '@assets/icons/logo.svg';
-import infoIcon from '@assets/icons/info.svg';
-import { menu, newsCounts } from './data/menu';
+import { Link, useLocation } from "react-router-dom";
+import logoSrc from "@assets/icons/logo.svg";
+import infoIcon from "@assets/icons/info.svg";
+import { menu, newsCounts } from "./data/menu";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 
 export const Sidebar = () => {
   const location = useLocation();
@@ -21,26 +21,24 @@ export const Sidebar = () => {
           {menu.map(({ iconSrc, href, label }) => {
             const isActive = location.pathname === href;
             const count = newsCounts.find(
-              (item) => item.path === href.split('/')[1]
+              (item) => item.path === href.split("/")[1]
             )?.count;
 
             return (
               <Link to={href} key={href} className="sidebar-menu-item-link">
                 <div
-                  className={`sidebar-menu-item ${isActive ? 'active' : ''}`}
+                  className={`sidebar-menu-item ${isActive ? "active" : ""}`}
                 >
                   <img
                     src={iconSrc}
                     alt={label}
                     className="sidebar-menu-item-icon"
                   />
-                  <span className="sidebar-menu-label">
-                    {label}
+                  <span className="sidebar-menu-label">{label}</span>
 
-                    {count && (
-                      <div className="sidebar-menu-counter-icon">{count}</div>
-                    )}
-                  </span>
+                  {count && (
+                    <div className="sidebar-menu-counter-icon">{count}</div>
+                  )}
                 </div>
               </Link>
             );
